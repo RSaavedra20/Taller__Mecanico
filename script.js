f953e0c9e0fb3f8824446fcfe1c93b8ab1083a55
Rodrigo <ro.saavedrap@duocuc.cl>
2022-05-17 19:07:57 -0400

    mapa

---
+++ script.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:-33.348869 ,lng: -70.695872};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
